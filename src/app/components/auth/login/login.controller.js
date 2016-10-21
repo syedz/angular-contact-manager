188bd3df65d9315858d71de098858fb26cd18fef
Zeeshan Syed <zsyed89@gmail.com>
2016-10-21 05:10:20 -0400

    Basic register and login pages

---
+++ src/app/components/auth/login/login.controller.js
@@ -0,0 +1,11 @@
+function LoginController() {
+  var ctrl = this;
+
+  ctrl.loginUser = function () {
+
+  };
+}
+
+angular
+  .module('components.auth')
+  .controller('LoginController', LoginController);
--- src/app/root.module.js
+++ src/app/root.module.js
@@ -1,4 +1,5 @@
 angular
   .module('root', [
+    'components',
     'templates'
   ]);
