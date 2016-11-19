88784b5e67468aeeeb101e2056121d10166933c4
Zeeshan Syed <zsyed89@gmail.com>
2016-11-19 15:11:36 -0500

    Added contact list and link to contact details

---
+++ src/app/components/contact/contacts/contacts.controller.js
@@ -0,0 +1,13 @@
+function ContactsController($state) {
+  var ctrl = this;
+
+  ctrl.goToContact = function(event) {
+    $state.go('contact', {
+      id: event.contactId
+    });
+  };
+}
+
+angular
+  .module('components.contact')
+  .controller('ContactsController', ContactsController);
